114d70bf23f0f50178050b2202d9f97658546dde
lycho33 <cho.lyds@gmail.com>
2021-02-08 13:28:24 -0500

    commit tree 15c42d1c6583dfaa9d8b6fbdaf60f6982a1ba1c9

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.each.with_index(1) {|dwarf, index| puts "#{index}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
   # Your code here
+  planeteer_calls.collect {|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)
   # Your code here
+  if words.length > 4
+    false
+  else
+    true
+  end
 end
 
-def find_the_cheese# code an argument here
+
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if list.include?("cheddar")
+    cheese_types[0]
+  else
+    nil
+  end
 end
